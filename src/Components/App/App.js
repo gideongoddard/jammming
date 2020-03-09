d98889aedf4b49f77eed4846a2795a0b26fb2a1e
Gideon Goddard <goddard.gcw@gmail.com>
2020-03-09 13:05:46 +0000

    Progressed project up to the stage of having track component pulling in props from app.js search results array.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,22 +1,54 @@
 import React from 'react';
 import './App.css';
-import { SearchBar } from '../SearchBar/SearchBar';
-import { SearchResults } from '../SearchResults/SearchResults';
-import { Playlist } from '../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{
+        name: 'Tiny Dancer',
+        artist: 'Elton John',
+        album: 'Madman Across The Water',
+        id: 1,
+      },
+      {
+        name: 'Tiny Dancer',
+        artist: 'Tim McGraw',
+        album: 'Love Story',
+        id: 2,
+      },
+      {
+        name: 'Tiny Dancer',
+        artist: 'Rockabye Baby!',
+        album: 'Lullaby Renditions of Elton John',
+        id: 3,
+      },
+      {
+        name: 'Tiny Dancer',
+        artist: 'The White Raven',
+        album: 'Tiny Dancer',
+        id: 4,
+      }],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Track.css';
 
-export class Track extends React.Component {
+class Track extends React.Component {
     renderAction() {
         if (this.props.isRemoval) {
             return <button className="Track-action">-</button>
@@ -14,11 +14,13 @@ export class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3>track name</h3>
-                    <p>track artist | track album</p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 {this.renderAction()}
             </div>
         );
     }
 }
+
+export default Track;
