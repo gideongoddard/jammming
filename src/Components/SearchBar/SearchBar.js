89c13ee90beaa502d82feb2d2899e4d8ff00a6b9
Gideon Goddard <goddard.gcw@gmail.com>
2020-03-13 16:36:23 +0000

    Added an onClick attribute to the search button in the Search Bar module to pass the search term into the search method in app.js

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,11 +2,26 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(event) {
+        this.setState({term: event.target.value});
+    }
+
+    // Render method has the onClick attribute added to the button element. This isn't covered by the tasklist.
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+                <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         );
     }
