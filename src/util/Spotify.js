a82b9358d9b30d2cbb95bce56701d3678c9cbdb3
Gideon Goddard <goddard.gcw@gmail.com>
2020-04-03 11:02:15 +0100

    Created Spotify savePlaylist() method. WIP.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -49,6 +49,27 @@ const Spotify = {
                 uri: track.uri
             }));
         })
+    },
+
+    savePlaylist(playlistName, trackUris) {
+        if (!playlistName || !trackUris.length) {
+            return;
+        }
+
+        const accessToken = Spotify.getAccessToken();
+        const headers = { Authorization: `Bearer ${accessToken}` };
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', {headers: headers}
+        ).then(response => response.json()
+        ).then(jsonResponse => {
+            userId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({ name: playlistName})
+            })
+        });
     }
 }
 
