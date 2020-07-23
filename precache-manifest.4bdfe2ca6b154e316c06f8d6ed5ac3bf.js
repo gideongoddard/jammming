d378c0714545c389fdc0acbf48c1263403df2d69
Gideon Goddard <goddard.gcw@gmail.com>
2020-07-23 12:07:22 +0100

    Updates

---
+++ precache-manifest.4bdfe2ca6b154e316c06f8d6ed5ac3bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7883e8c8cef876e3d603ac116e100a7",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "06df113a881597369d4f",
+    "url": "/jammming/static/css/main.fca10e4b.chunk.css"
+  },
+  {
+    "revision": "a105b4dce9ac8786f282",
+    "url": "/jammming/static/js/2.e0eb56a3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/jammming/static/js/2.e0eb56a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06df113a881597369d4f",
+    "url": "/jammming/static/js/main.04b4e091.chunk.js"
+  },
+  {
+    "revision": "3cead86a956098c6f441",
+    "url": "/jammming/static/js/runtime-main.3a5ccc2d.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
